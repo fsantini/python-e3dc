8e7c16782e00055a943f821f9c660221cff33442
Alexander Meisel <alex@otrky.com>
2020-07-01 09:40:14 +0200

    Add stringIndex to get_pvi_data to support more than just string 0

--- e3dc/_e3dc.py
+++ e3dc/_e3dc.py
@@ -784,12 +784,13 @@ class E3DC:
             }
         return outObj
 
-    def get_pvi_data(self, keepAlive = False):
+    def get_pvi_data(self, stringIndex = 0, keepAlive = False):
         """Polls the inverter data via rscp protocol locally
         
         Returns:
             Dictionary containing the pvi data structured as follows:
                 {
+                    'stringIndex': string inde
                     'acApparentPower': ac apparent power
                     'acCurrent': ac current
                     'acEnergyAll': ac energy all
@@ -807,7 +808,7 @@ class E3DC:
                 }
         """  
         req = self.sendRequest( ('PVI_REQ_DATA', 'Container', [ 
-            ('PVI_INDEX', 'Uint16', 0), 
+            ('PVI_INDEX', 'Uint16', stringIndex),
             ('PVI_REQ_TEMPERATURE', 'None', None),
             ('PVI_REQ_AC_VOLTAGE', 'None', None),
             ('PVI_REQ_AC_CURRENT', 'None', None),
@@ -841,6 +842,7 @@ class E3DC:
 
 
         outObj = {
+            'stringIndex': stringIndex,
             'acApparentPower': acApparentPower,
             'acCurrent': acCurrent,
             'acEnergyAll': acEnergyAll,
