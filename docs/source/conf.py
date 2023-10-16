613e52768bfc63161dfd5a1273ae465da23885a9
Christopher Banck <vchrisb@users.noreply.github.com>
2023-10-16 11:54:06 +0200

    remove html_static_path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ language = "en"
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
 
 # -- Options for todo extension ----------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/extensions/todo.html#configuration
